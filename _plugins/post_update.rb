5cb6436aaeaf4782e00c5114ce062af17d390d7d
Oliver Roick <oliver.roick@gmail.com>
2020-06-13 16:25:36 +0100

    feat: Add custom update tag

---
+++ _plugins/post_update.rb
@@ -0,0 +1,12 @@
+require 'kramdown'
+
+module Jekyll
+  class PostUpdate < Liquid::Block
+    def render(context)
+      text = super
+      "<div class=\"update\">#{Kramdown::Document.new(text).to_html}</div>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('post_update', Jekyll::PostUpdate)
